ad8dd72b3f0d4dc6dfcdc263af63d85426d357ee
roy <76505705+2703roy@users.noreply.github.com>
2025-01-19 10:22:45 +0700

    new app test
    new app test
    
    Signed-off-by: roy <76505705+2703roy@users.noreply.github.com>

---
+++ hello-rust/target/debug/deps/hello_rust-e280c493f7f9bf2f.d
@@ -0,0 +1,5 @@
+/mnt/d/workspace/hello-rust/target/debug/deps/hello_rust-e280c493f7f9bf2f: src/main.rs
+
+/mnt/d/workspace/hello-rust/target/debug/deps/hello_rust-e280c493f7f9bf2f.d: src/main.rs
+
+src/main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/mnt/d/workspace/hello-rust/target/debug/hello-rust: /mnt/d/workspace/hello-rust/src/main.rs
